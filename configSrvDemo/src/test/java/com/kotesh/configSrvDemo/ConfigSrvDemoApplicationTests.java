0f2043cb2176873d97f99f21b300763cf852f958
Koteswar Chandra <Koteswar.Chandra@fadv.com>
2019-01-25 19:12:51 +0530

    config

---
+++ configSrvDemo/src/test/java/com/kotesh/configSrvDemo/ConfigSrvDemoApplicationTests.java
@@ -0,0 +1,17 @@
+package com.kotesh.configSrvDemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ConfigSrvDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
